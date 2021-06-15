5d1dd6b3f62aa9bec75727f8aa2c850f642ba1a2
albertec1 <45201952+albertec1@users.noreply.github.com>
2021-06-15 21:32:35 +0200

    it all came together again
    New unit, improved round behavior -- Oscar
    A-B buttons on right controller detected -- Albert

--- Eldrich Invasion/Assets/Scripts/WaveManager.cs
+++ Eldrich Invasion/Assets/Scripts/WaveManager.cs
@@ -1,58 +1,136 @@
 
+using System.Collections;
+using System.Runtime.ExceptionServices;
 using System.Security.Cryptography;
 using UnityEngine;
 
 public class WaveManager : MonoBehaviour
 {
 
-    public Transform enemyPrefab;
-
-    public Transform spawnArea;
-    public float waveCooldown = 10f;
-    public float Radius = 1f;
     public Vector3 center;
     public Vector3 size;
 
-    private float countdown = 5f;
-    private int waveNumer = 1;
+    public enum SpawnState
+    {
+        SPAWNING,
+        WAITING,
+        COUNTING
+    };
 
-    
+    [System.Serializable]
+    public class Wave
+    {
+        public string name;
+        public Transform enemy;
+        public Transform enemy2;
+        public int count;
+        public int count2;
+        public float rate;
+    }
+
+    public Wave[] waves;
+    private int nextWave = 0;
+
+    public float timeBetweenWaves = 5f;
+    public float waveCountdown;
+
+    private float searchCountdown = 1f;
+
+    private SpawnState state = SpawnState.COUNTING;
+
+    void Start()
+    {
+        waveCountdown = timeBetweenWaves;
+    }
+
 
     void Update()
     {
-        if (countdown <= 0f)
+        if(state == SpawnState.WAITING)
         {
-            SpawnWave();
+            if (!EnemyAlive())
+            {
+                WaveCompleted();
+            }
+            else
+            {
+                return;
+            }
+        }
+
+        if(waveCountdown <= 0)
+        {
+            if(state != SpawnState.SPAWNING)
+            {
+                StartCoroutine(SpawnWave(waves[nextWave]));
+            }
             
-            countdown = waveCooldown;
         }
-
-        countdown -= Time.deltaTime;
-        
-    }
-
-    void SpawnWave()
-    {
-        Debug.Log("Wave incoming");
-
-        for (int i = 0; i<waveNumer; i++)
+        else
         {
-            SpawnEnemy();
+            waveCountdown -= Time.deltaTime;
         }
 
-        waveNumer++;
     }
 
-    void SpawnEnemy()
+    void WaveCompleted()
     {
-        //enemyPrefab.position
+        state = SpawnState.COUNTING;
+        waveCountdown = timeBetweenWaves;
+
+        if(nextWave + 1 > waves.Length -1 )
+        {
+            nextWave = 0;
+        }
+        else
+        {
+            nextWave++;
+        }
+
+    }
+
+    bool EnemyAlive()
+    {
+
+        searchCountdown -= Time.deltaTime;
+        if (searchCountdown <= 0f)
+        {
+            searchCountdown = 1f;
+            if (GameObject.FindGameObjectsWithTag("Enemy").Length == 0)
+            {
+                Debug.Log("Wave completed AAAAAAAAAAAAAAAAAAAAAA");
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    IEnumerator SpawnWave (Wave _wave)
+    {
+        state = SpawnState.SPAWNING;
+
+        for(int i = 0; i<_wave.count; i++)
+        {
+            SpawnEnemy(_wave.enemy);
+            yield return new WaitForSeconds(1f/ _wave.rate);    
+        }
+        for (int i = 0; i < _wave.count2; i++)
+        {
+            SpawnEnemy(_wave.enemy2);
+            yield return new WaitForSeconds(1f / _wave.rate);
+        }
+
+
+        state = SpawnState.WAITING;
+
+        yield break;
+    }
+
+    void SpawnEnemy(Transform _enemy)
+    {
         Vector3 randomPos = new Vector3(-10.6f, 1.5f, 121) + new Vector3(Random.Range(-2, 2), 0, Random.Range(-7, 7));
-        Instantiate(enemyPrefab, randomPos, Quaternion.identity);
+        Instantiate(_enemy, randomPos, Quaternion.identity);
     }
 
-    private void OnDrawGizmos()
-    {
-        Gizmos.color = Color.red;
-        Gizmos.DrawCube(center, size);
-    }
 }
